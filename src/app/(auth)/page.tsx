b728fe9e099c0fdfa635e33507101e60d27b4c16
hirokawa00 <monolab000@gmail.com>
2024-10-01 13:52:17 +0900

    強制的にssr

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -1,21 +1,21 @@
-import { ContentLayout } from '@/components/admin-panel/content-layout';
-
 import { ProductCard } from '@/app/(auth)/components/product-card';
 import { SalesCard } from '@/app/(auth)/components/sales-card';
 import { StoreCard } from '@/app/(auth)/components/store-card';
 import { UserCard } from '@/app/(auth)/components/user-card';
-
+import { ContentLayout } from '@/components/admin-panel/content-layout';
 import { HourlySalesTodayChart } from './components/hourly-sales-today-chart';
 import { ProductCategorySalesPieChart } from './components/product-category-sales-pie-chart';
 import { SalesForecastLineChart } from './components/sales-forecast-chart';
-
 import { WeeklySalesChart } from './components/weekly-sales-chart';
+
+// サーバーコンポーネントで SSR を強制
+export const dynamic = 'force-dynamic';
 
 export default function Home() {
   return (
     <ContentLayout title="Home">
       <div className="min-h-screen p-6">
-        <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
+        <h1 className="text-2xl font-bold mb-4">Dashboardd</h1>
 
         {/* 各種情報カード */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  gap-4">
